fccec1f99ed596e8c771bd847630c11a9c9f94e7
Anand Srinivasan <asrini@mit.edu>
2018-02-12 19:59:45 -0800

    sweep_params.py changes

--- sweep_params.py
+++ sweep_params.py
@@ -9,20 +9,23 @@ def getCmd(epochs, learning_rate, batch_size, delay, seed):
 	return 'python main.py --epochs={} --lr={} --batch-size={} --delay={} --seed={} --tensorboard-plot=grid_search --log-output'.format(epochs, learning_rate, batch_size, delay, seed)
 
 
-seed = 1
+# seed = 1
+seed = 500
 
-for delay in range(10):
+# for delay in range(10):
+for delay in range(2, 10, 2):
 
-	for lr_exponent in np.linspace(-5, 2, num=15):
+	# for lr_exponent in np.linspace(-5, 2, num=15):
+	for lr_exponent in np.linspace(-5, -.5, num=10):
 		lr = 10 ** lr_exponent
 
-		for batch_exponent in range(6, 12):
+		for batch_exponent in range(6, 11):
 			bs = 2 ** batch_exponent
 
 			cmd = getCmd(epochs=30, learning_rate=lr, batch_size=bs, delay=delay, seed=seed)
 
 			print(cmd)
-			
+
 			subprocess.call(cmd, shell=True)
 
 			seed += 1
